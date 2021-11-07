c5e30296139af380e0259979a97529c473468a35
anshita0911 <80672585+anshita0911@users.noreply.github.com>
2021-11-07 17:38:44 +0530

    new

---
+++ index.js
@@ -0,0 +1,2 @@
+function add_chatinline(){var hccid=37348320;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
+  add_chatinline();
